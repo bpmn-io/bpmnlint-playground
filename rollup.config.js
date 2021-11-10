434b13ac631f9addaca7de7af81914a07314aaa4
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-11-10 12:35:23 +0100

    chore: provide default lint configuration

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,46 @@ import copy from 'rollup-plugin-copy';
 
 import { string } from 'rollup-plugin-string';
 
+import bpmnlint from 'rollup-plugin-bpmnlint';
+
 const publicDir = 'public';
 
-export default {
-  input: 'src/client.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'BpmnlintPlayground',
-    file: `${publicDir}/client.js`
+export default [
+  {
+    input: 'src/client.js',
+    external: [ './bpmnlint-config.js'],
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'BpmnlintPlayground',
+      file: `${publicDir}/client.js`
+    },
+    plugins: [
+      string({
+        include: '**/*.bpmn'
+      }),
+      resolve(),
+      commonjs(),
+      copy({
+        targets: [
+          { src: 'src/index.html', dest: publicDir },
+          { src: 'node_modules/bpmn-js/dist/assets', dest: `${publicDir}/assets/bpmn-js` },
+          { src: 'node_modules/bpmn-js-bpmnlint/dist/assets', dest: `${publicDir}/assets/bpmn-js-bpmnlint` }
+        ]
+      })
+    ]
   },
-  plugins: [
-    string({
-      include: '**/*.bpmn'
-    }),
-    resolve(),
-    commonjs(),
-    copy({
-      targets: [
-        { src: 'src/index.html', dest: publicDir },
-        { src: 'node_modules/bpmn-js/dist/assets', dest: `${publicDir}/assets/bpmn-js` },
-        { src: 'node_modules/bpmn-js-bpmnlint/dist/assets', dest: `${publicDir}/assets/bpmn-js-bpmnlint` }
-      ]
-    })
-  ]
-};
+  {
+    input: 'src/.bpmnlintrc',
+    output: {
+      sourcemap: true,
+      format: 'es',
+      file: `${publicDir}/bpmnlint-config.js`
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      bpmnlint()
+    ]
+  }
+];
