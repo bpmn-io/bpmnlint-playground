45c302c948a42fe7edfece7181ec4ab5397320eb
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-26 15:10:33 +0100

    test: extend example to feature custom package
    Related to https://github.com/bpmn-io/bpmnlint-playground/issues/36

--- example/bpmnlint-plugin-local/test.js
+++ example/bpmnlint-plugin-local/test.js
@@ -1,11 +1,16 @@
 const { expect } = require('chai');
 
+const spellModdle = require('spell-moddle/resources/spell.json');
+
 const { createModdle } = require('bpmnlint/lib/testers/helper');
 
 const RuleTester = require('bpmnlint/lib/testers/rule-tester');
 
 const manualTaskRule = require('./rules/no-manual-task');
 const targetNamespaceRule = require('./rules/target-namespace');
+const magicStartEventRule = require('./rules/magic-start-event');
+
+const BpmnModdle = require('bpmn-moddle');
 
 
 RuleTester.verify('no-manual-task', manualTaskRule, {
@@ -48,6 +53,45 @@ RuleTester.verify('target-namespace', targetNamespaceRule, {
       report: {
         id: 'definitions',
         message: 'Element is missing targetNamespace'
+      }
+    }
+  ]
+});
+
+async function createMagicModdle(xml, elementType) {
+
+  const moddle = new BpmnModdle({ spell: spellModdle });
+
+  const {
+    rootElement,
+    warnings
+  } = await moddle.fromXML(xml, elementType || 'bpmn:Definitions');
+
+  return {
+    root: rootElement,
+    moddle,
+    warnings
+  };
+}
+
+RuleTester.verify('magic-start-event', magicStartEventRule, {
+  valid: [
+    {
+      moddleElement: createMagicModdle(
+        '<startEvent xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:s="http://example.magic/schema/spell" id="startEvent" s:magic="true" />',
+        'bpmn:StartEvent'
+      )
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: createMagicModdle(
+        '<startEvent xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" id="startEvent" />',
+        'bpmn:StartEvent'
+      ),
+      report: {
+        id: 'startEvent',
+        message: 'Element must be magic'
       }
     }
   ]
